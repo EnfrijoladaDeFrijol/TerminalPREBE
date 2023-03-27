778d8b8fdf5d5725bf99933f3012b0ec7e64a4a7
Arturo Meza <arturomezsanchez@gmail.com>
2023-03-27 00:55:36 -0600

    [FEATURE] Modificacion de colores

--- sistemaAcceso.sh
+++ sistemaAcceso.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+R='\033[1;31m' # Rojo
+G='\033[1;32m' # Verde
+Y='\033[1;33m' # Amarillo
+B='\033[1;34m' # Azul
+M='\033[1;35m' # Mangenta
+W='\033[0m' # Blanco
+Glig='\e[1;32m' # Verde claro
+
+
 # Sistema de acceso para los usuarios
+# ==-==-==-==-==-==-==-==-==-==-==-==-
 clear
 
 printf "\t  _____              _           _   ___ ___ ___ ___ ___ \n"
@@ -9,8 +19,23 @@ printf "\t   | |/ -_) '_| '  \| | ' \/ _' | | |  _/   / _|| _ \ _| \n"
 printf "\t   |_|\___|_| |_|_|_|_|_||_\__,_|_| |_| |_|_\___|___/___|\n"
 printf "\n\t\t\tI n i c i o   d e   s e i ó n\n"
 
-printf "\n\t\t\t\tUsuario: "
+# ==-==-==-==-> Guardado de password en archivo
+#touch archivoPass.txt # Crea un archivo temporal
+#sudo cat /etc/passwd >> archivoPass.txt
+#cat archivoPass.txt
+
+# ==-==-==-==-> Solicitud de datos (interacion con el usuario)
+printf "$G\n\t\t\t\tUsuario: $W"
 read usuario
 #echo $usuario
-printf "\n\t\t\t     Contraseña: "
+printf "$G\n\t\t\t     Contraseña: $W"
 read contrasena
+#echo $contrasena
+
+# ==-==-==-==-> Validación de datos
+if [ $usuario == $USER ]
+then
+	printf "Usuario correcto\n\n"
+else
+	printf "Usuario incorrecto\n\n"
+fi
