098b417d531637e1ede401d2a7b2e85c7a32c875
Arturo Meza <arturomezsanchez@gmail.com>
2023-04-15 02:47:13 -0600

    [FEATURE] Creación del script que da fecha y hora

---
+++ tiempo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Colores para el texto
+R='\033[1;31m' # Rojo
+G='\033[1;32m' # Verde
+B='\033[1;34m' # Azul
+G='\033[1;32m' # Verde
+Y='\033[1;33m' # Amarillo
+Glig='\e[1;32m' # Verde claro
+W='\033[0m' # Blanco
+
+imprimirTitulo(){
+	printf "\t  _____ _                      \n"
+	printf "\t |_   _(_)___ _ __  _ __  ___  \n"
+	printf "\t   | | | / -_) '  \| '_ \/ _ | \n"
+	printf "\t   |_| |_\___|_|_|_| .__/\___/ \n"
+	printf "\t                   |_|         \n"
+}
+
+obtenerFechaHora(){
+	fecha=$(date +"%Y-%m-%d")
+	hora=$(date +"%H:%M:%S2")
+}
+
+imprimirFechaHora(){
+	printf "$Y\n\t\tFecha:$W $fecha\n"
+	printf "$Y\n\t\tHora:$W $hora\n"
+}
+
+main(){
+	clear
+	imprimirTitulo
+	obtenerFechaHora
+	imprimirFechaHora
+	printf "\n\t    'Que tenga un bonito día :)' \n\n"
+}
+
+main
