bd6d8ccd112e481d0f44e24809d2b12660172c01
Arturo Meza <arturomezsanchez@gmail.com>
2023-04-18 03:53:44 -0600

    [FEATURE] Se puso todo dentro de un if

--- interaccion.sh
+++ interaccion.sh
@@ -13,9 +13,39 @@ obtenerUserCarpeta(){
 	dir=$(pwd) #Obtiene la ubicación actual
 }
 
-imprimirLeerLineaComandos(){
-	printf "\n$R$usuario$W:$Y$dir$W-$ "
+imprimirLineaComandos(){
+	printf "$R$usuario$W:$Y$dir$W-$ "
+}
+
+leerComando(){
 	read entrada
+}
+
+mensajeDespedida(){
+	printf "\n"
+	printf "\t\t   (                 \n"
+	printf "\t\t ( )\   (        (   \n"
+	printf "\t\t )((_)  )\ )    ))\  \n"
+	printf "\t\t((_)_  (()/(   /((_) \n"
+	printf "\t\t | _ )  )(_)) (_))   \n"
+	printf "\t\t | _ \ | || | / -_)  \n"
+	printf "\t\t |___/  \_, | \___|  \n"
+	printf "\t\t        |__/         \n"
+	printf "\n\t\tS a l i e d o   d e \n"
+	printf "\t    T E R M I N A L   P R E B E\n"
+	printf "\t\t\te n :\n\n"
+}
+
+cuentaAtras(){
+	printf "\t"
+	for i in 3 2 1 
+	do
+		clear
+		imprimirLineaComandos
+		mensajeDespedida
+		printf "\n\t\t\t $i\n\n\n\n\n\n"
+		sleep 1.5
+	done
 }
 
 main(){
@@ -24,56 +54,60 @@ main(){
 
 	while true
 	do
+    	imprimirLineaComandos
+    	leerComando
 
-    	imprimirLeerLineaComandos
-
-    	# Limpiar pantalla
+    	# 1. Limpiar pantalla
     	if [[ "$entrada" == "limpiar" ]]
     	then
     		clear
-    	fi
-
-    	
-    	# Salir si el usuario escribe "salir"
-    	if [[ "$entrada" == "salir" ]]
+    	# 2. Salir si el usuario escribe "salir"
+    	elif [[ "$entrada" == "salir" ]]
     	then
-    		printf "\n HASTA LUEGO\n\n"
+    		printf "\n"
+    		cuentaAtras
+    		clear
+    		cat rei.txt
+    		sleep 0.5
+    		clear
     		exit 0
-    	fi
-
-    	# Mostrar información del sistema
-    	if [[ "$entrada" == "infosis" ]]
+    	# 3. Mostrar información del sistema
+    	elif [[ "$entrada" == "infosis" ]]
     	then
     		./infosis.sh
-    	fi
-
-		# Mostrar la fecha y hora
-    	if [[ "$entrada" == "tiempo" ]]
+		# 4. Mostrar la fecha y hora
+    	elif [[ "$entrada" == "tiempo" ]]
     	then
     		./tiempo.sh
-    	fi
-
-		# Mostrar ayuda de comandos ERROR
-    	if [[ "$entrada" == "ayuda" ]]
+		# 5. Mostrar ayuda de comandos ERROR
+    	elif [[ "$entrada" == "ayuda" ]]
     	then
     		clear
     		cat ayuda.txt
     		printf "\n\n"
-    	fi
-
-    	# EJecuta el juego
-    	if [[ "$entrada" == "jugar" ]]
+    	# 6. EJecuta el juego
+    	elif [[ "$entrada" == "jugar" ]]
     	then
     		./juegoPREBE.sh
-    	fi
-
-    	# EJecuta el juego
-    	if [[ "$entrada" == "creditos" ]]
+    	# 7. EJecuta el juego
+    	elif [[ "$entrada" == "creditos" ]]
     	then
     		clear
     		cat creditosProgramador.txt
     		printf "\n\n"
+    	# 8 Busqueda de un archivo
+    	elif [[ "$entrada" == "buscar" ]] 
+    	then
+    		./buscar.sh
+    	# Extra. Easter egg
+    	elif [[ "$entrada" == "capybara" ]]
+    	then
+    		cat capybara.txt
+    		printf "\n\n"
+    	else
+    		printf "$B[Advertencia]$W $entrada: comando no válido.\n"
     	fi
+
 
 		# Ejecutar el entrada ingresado
 	    #salida=$(eval "$entrada" 2>&1)
